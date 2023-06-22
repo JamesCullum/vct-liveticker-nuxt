e74e259f9043f548574ddac3b07d239d2f9e6240
James Cullum (Pseudonym) <JamesCullum@users.noreply.github.com>
2023-06-22 20:30:53 +0200

    Add manifests & SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,41 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: "Liveticker for VCT",
+      title: "Ticker for VCT",
       meta: [
         {
           name: "description",
           content: "Follow the matches of the Valorant Champions Tour live",
+        },
+        {
+          name: "msapplication-TileColor",
+          content: "#da532c",
+        },
+        {
+          name: "theme-color",
+          content: "#47b3ff",
         },
       ],
       script: [
         {
           src: "https://onesignal-proxy.jamescullum.workers.dev/sdks/OneSignalSDK.js",
           defer: true,
+        },
+      ],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/img/apple-touch-icon.png",
+        },
+        { rel: "icon", sizes: "32x32", href: "/img/favicon-32x32.png" },
+        { rel: "icon", sizes: "16x16", href: "/img/favicon-16x16.png" },
+        { rel: "manifest", href: "/site.webmanifest" },
+        {
+          rel: "mask-icon",
+          href: "/img/safari-pinned-tab.svg",
+          color: "#47b3ff",
         },
       ],
     },
